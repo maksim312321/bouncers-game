b978a186ea0c7bc418d19e5481a14e523983c444
Maksim Zelenin <maksim.zelenin.01@bk.ru>
2021-07-08 23:29:53 +0500

    описал рандомное движение цели

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const gameField = document.getElementById("game-field");//игровое поле 
-const enemy = document.getElementById("enemy")//
+const enemy = document.getElementById("enemy")//цель(на которую нужно тыкнуть)
+
+
+let enemySize = 200
+enemy.style.width  = enemySize+"px";
+enemy.style.height  = enemySize+"px";
+setInterval(function(){
+     enemy.style.left = (Math.floor(Math.random( ) * (gameField.clientHeight - enemySize)))+ "px";
+     enemy.style.top = (Math.floor(Math.random( ) * (gameField.clientHeight - enemySize)))+ "px";
+     }, 300);
+
+
+
 
 gameField.addEventListener("click", (event)=>{
-    console.dir(event);
+    if(event.target == enemy){
+        console.log("попал!");
+
+    }
+    else{
+        console.log("не попал!");
+
+    };
 })
