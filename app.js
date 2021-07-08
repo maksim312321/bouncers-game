17918d363d40090e1b5d363b3de64e09595f97cf
Maksim Zelenin <maksim.zelenin.01@bk.ru>
2021-07-08 22:59:15 +0500

    создал переменные поля и цели

---
+++ app.js
@@ -0,0 +1,6 @@
+const gameField = document.getElementById("game-field");//игровое поле 
+const enemy = document.getElementById("enemy")//
+
+gameField.addEventListener("click", (event)=>{
+    console.dir(event);
+})
