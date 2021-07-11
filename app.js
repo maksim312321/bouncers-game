6a2a72717541213eb1312dd335207db25dea62d0
Maksim Zelenin <maksim.zelenin.01@bk.ru>
2021-07-11 23:05:56 +0500

    проигрыш(тыкнул мимо)

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ let score = document.getElementById("score");//счет
 let levels = document.getElementById("levels");//блок выбора уровней
 let levelsPresent = document.getElementsByClassName("precent");
 let interval_id;
+let timeout_id;
 let count;
 let precent;//процент прохждения
 
-for(let i=0; i<5; i++){//вывод из локального хранилища процент прохождения игры
+levelsPresent[0].innerHTML = localStorage.getItem(0, precent);
+for(let i=1; i<6; i++){//вывод из локального хранилища процент прохождения игры
     if(localStorage.getItem(i, precent)){
     levelsPresent[i].innerHTML = localStorage.getItem(i, precent) + "%";
     }
 }
 
 function resetPresent(){
+    levelsPresent[0].innerHTML = 0;
     localStorage.clear();
-    for(let i=0; i<5; i++){//вывод из локального хранилища процент прохождения игры
+    for(let i=1; i<6; i++){//вывод из локального хранилища процент прохождения игры
         levelsPresent[i].innerHTML = 0 + "%";
     }
 }
@@ -26,15 +29,6 @@ function startPosition(){//ф-я возврата на стартовую поз
     score.innerHTML = 0;
 };
 
-
-function hit(event){ //ф-я попадания по элементу
-    if(event.target == enemy){
-        enemy.style.display = "none" //скрыть элемент, если по нему попали
-        count = count + 1;
-        score.innerHTML = count;
-        console.log("счет" + count)
-    }
-};
 
 function result(precent){ //Текстовый вывод результата
     if(precent>=80){
@@ -50,33 +44,55 @@ function result(precent){ //Текстовый вывод результата
         return(" Пробуй еще, у тебя получится!")
     }
     
-}
+};
 
-console.log(levelsPresent);
 function gameStart(enemySize, time, level){//ф-я начала игры
     levels.style.display = "none";
     count = 0;//счет 
     enemy.style.width  = enemySize+"px";
     enemy.style.height  = enemySize+"px";
-    setTimeout(gameEnd, 10000)
+    if (level!= 0){ // если выбран уровень с 1-5 ставим таймер, иначе свободный режим игры б
+    timeout_id = setTimeout(gameEnd, 10000)
+    }
     interval_id = setInterval(function(){
      enemy.style.display = "block"//отобразить блок
      enemy.style.left = (Math.floor(Math.random( ) * (gameField.clientWidth - enemySize)))+ "px"; //рандом положения объекта по горизонтале
      enemy.style.top = (Math.floor(Math.random( ) * (gameField.clientHeight - enemySize)))+ "px";//рандом положения объекта по вертикале
 
      }, time);
-    gameField.addEventListener("click", hit);
 
-    function gameEnd(){//окончание игры
+     function gameEnd(lose){//окончание игры
         startPosition(); 
         clearInterval(interval_id);
-        precent = Math.round((count/Math.floor((10000-time)/time))*100);
-        alert("Точность: " + precent + "%."+ result(precent));
-        localStorage.setItem(level-1, precent);
-        levelsPresent[level-1].innerHTML = localStorage.getItem(level-1, precent) + "%";
+        if(lose){ //если игра проиграна (то есть допущен промах)
+            clearTimeout(timeout_id);//очищаем таймер окончания игры
+            alert(lose);//выводим соответсвующую надпись
+            if(level == 0){
+                localStorage.setItem(level, count);//запись в локальное хранилище рекорда по очкам
+                levelsPresent[level].innerHTML = localStorage.getItem(level, count);// вывод на экран рекорд по очками
+            }
+        }
+        else{//игра закончена по таймеру
+            precent = Math.round((count/Math.floor((10000-time)/time))*100);//расчет процента попадания
+            alert("Попадания: " + precent + "%."+ result(precent));
+            localStorage.setItem(level, precent);//запись в локальное хранилище процента прохождения игры
+            levelsPresent[level].innerHTML = localStorage.getItem(level, precent) + "%";//вывод на экран процента прохождения
+        }
         levels.style.display ="block";
         gameField.removeEventListener("click", hit)
     };
+
+     function hit(event){ //ф-я попадания по элементу
+        if(event.target == enemy){
+            enemy.style.display = "none" //скрыть элемент, если по нему попали
+            count = count + 1;
+            score.innerHTML = count;
+        }
+        else{
+            gameEnd("Вы промахнулись! Попробуйте еще раз.");
+        }
+    }
+    gameField.addEventListener("click", hit);//попадание по элементу
 };
 
 
